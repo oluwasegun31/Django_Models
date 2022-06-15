52f46329fd564630b580cc2cb6eb34acaf4d7d47
oluwasegun31 <oluwasegun1412@gmail.com>
2022-06-15 21:00:58 +0100

    first commit

---
+++ Olowosoyo/blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models import Model
+User = get_user_model()
+
+# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField()
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
